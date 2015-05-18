5668e1225398b93a14851d90a1e0f7f037daf6a0
thanhhuykg <thanhhuykg@gmail.com>
2015-05-18 10:26:54 +0000

    - At current position for slide page.

--- Gala-project/Gala/src/com/gala/layout/HomePageLayoutSlideImageMalls.java
+++ Gala-project/Gala/src/com/gala/layout/HomePageLayoutSlideImageMalls.java
@@ -7,12 +7,14 @@ import com.gala.app.R;
 
 import android.app.Activity;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class HomePageLayoutSlideImageMalls extends AbstractLayout{
 	
+	private int mCurrentPosition = 0;
 	@Override
 	public int getLayoutType() {
 		// TODO Auto-generated method stub
@@ -26,15 +28,35 @@ public class HomePageLayoutSlideImageMalls extends AbstractLayout{
 		
 		ViewPager vpImage = (ViewPager) v.findViewById(R.id.vpImage);
 
-		int pos = context.getIntent().getIntExtra("position", 0);
-
 		@SuppressWarnings("unchecked")
 		HomePageSlideImageMallsPagerAdapter sliAdapter = new HomePageSlideImageMallsPagerAdapter(context,
 				(ArrayList<String>) mDataSource
 				);
 		vpImage.setAdapter(sliAdapter);
 		// displaying selected image first
-		vpImage.setCurrentItem(pos);
+		vpImage.setCurrentItem(mCurrentPosition);
+		
+		vpImage.setOnPageChangeListener(new OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int position) {
+				// TODO Auto-generated method stub
+				mCurrentPosition = position;
+			}
+			
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 		return v;
 	}
 
