e8b0046a1b92cf8066fcc143d16454bef81088af
thanhhuykg <thanhhuykg@gmail.com>
2015-05-19 10:11:16 +0000

    add customview/CustomViewPagerSwipeAbleDisable.java

---
+++ Gala-project/Gala/src/com/gala/customview/CustomViewPagerSwipeAbleDisable.java
@@ -0,0 +1,42 @@
+package com.gala.customview;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class CustomViewPagerSwipeAbleDisable extends ViewPager{
+	private boolean enableSwipe = false;
+
+	public CustomViewPagerSwipeAbleDisable(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		setEnableSwipe(true);
+	}
+	
+	public CustomViewPagerSwipeAbleDisable(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+		setEnableSwipe(true);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		return enableSwipe && super.onTouchEvent(event);
+	}
+	
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		return enableSwipe && super.onInterceptTouchEvent(event);
+	}
+
+	public boolean isEnableSwipe() {
+		return enableSwipe;
+	}
+
+	public void setEnableSwipe(boolean enableSwipe) {
+		this.enableSwipe = enableSwipe;
+	}
+}
