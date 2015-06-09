61a3e11ac39317abc5e23d26c480c9a18e9fda51
thanhhuykg@gmail.com <thanhhuykg@gmail.com@51fc294b-89fd-9dc0-b554-f689dccb7375>
2015-06-09 10:09:36 +0000

    - Add main layout gcomm app.

---
+++ trunk/Gala-project/Gala_Use_Webview/gala/src/com/hopthanh/gala/adapter/MainSlideGridViewEDirectoryPagerAdapter.java
@@ -0,0 +1,102 @@
+package com.hopthanh.gala.adapter;
+
+import java.util.ArrayList;
+
+import com.hopthanh.gala.app.R;
+import com.hopthanh.gala.customview.CustomViewPagerWrapContent;
+import com.hopthanh.gala.customview.NonScrollableGridView;
+import com.hopthanh.gala.utils.AppConstant;
+import com.hopthanh.gala.utils.Utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.support.v4.view.PagerAdapter;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+
+public class MainSlideGridViewEDirectoryPagerAdapter extends PagerAdapter {
+
+	private Activity mActivity;
+	private ArrayList<ArrayList<String>> mDataSource;
+
+	// constructor
+	public MainSlideGridViewEDirectoryPagerAdapter(Activity activity,
+			ArrayList<ArrayList<String>> imagePaths) {
+		this.mActivity = activity;
+		this.mDataSource = imagePaths;
+	}
+
+	@Override
+	public int getCount() {
+		return this.mDataSource.size();
+	}
+
+	@Override
+	public boolean isViewFromObject(View view, Object object) {
+		return view == ((LinearLayout) object);
+	}
+
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		LayoutInflater inflater = (LayoutInflater) mActivity
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View viewLayout = inflater.inflate(
+				R.layout.main_layout_slide_non_scrollable_gridview_edirectory_view,
+				container, false);
+
+		NonScrollableGridView gridView = (NonScrollableGridView) viewLayout
+				.findViewById(R.id.gvEDirectory);
+
+		Resources r = mActivity.getResources();
+		float padding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+				AppConstant.GRID_PADDING, r.getDisplayMetrics());
+
+		float spacing = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+				AppConstant.GRID_SPACE, r.getDisplayMetrics());
+		
+		Utils utils = new Utils(mActivity.getApplicationContext());
+
+		int numOfColumns = AppConstant.NUM_OF_COLUMNS_PORTRAIT;
+		
+		if(mActivity.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+			numOfColumns = AppConstant.NUM_OF_COLUMNS_LANDSCAPE;
+		}
+		
+//		int columnWidth = (int) ((utils.getScreenWidth() - ((numOfColumns + 1) * padding)) / numOfColumns);
+		
+		int columnWidth = (int) ((utils.getScreenWidth() - 2 * spacing - (numOfColumns - 1) * spacing) / numOfColumns);
+		
+		gridView.setNumColumns(numOfColumns);
+		gridView.setColumnWidth(columnWidth);
+		gridView.setStretchMode(GridView.NO_STRETCH);
+		gridView.setPadding((int) spacing, (int) padding, (int) spacing,
+				(int) padding);
+		gridView.setHorizontalSpacing((int) spacing);
+		gridView.setVerticalSpacing((int) spacing);
+
+		// Gridview adapter
+		MainGridViewImageEDirectoryAdapter gvadapter = new MainGridViewImageEDirectoryAdapter(mActivity,
+				mDataSource.get(position), columnWidth);
+		//
+		// // setting grid view adapter
+		gridView.setAdapter(gvadapter);
+
+		((CustomViewPagerWrapContent) container).addView(viewLayout);
+		return viewLayout;
+	}
+
+	@Override
+	public void destroyItem(ViewGroup container, int position, Object object) {
+		LinearLayout ln = (LinearLayout) object;
+		((CustomViewPagerWrapContent) container).removeView(ln);
+		ln = null;
+
+	}
+
+}
---
+++ trunk/Gala-project/Gala_Use_Webview/gala/src/com/hopthanh/gala/utils/AppConstant.java
@@ -0,0 +1,35 @@
+package com.hopthanh.gala.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AppConstant {
+	// Number of styles.
+//	public static final int NUM_OF_STYLES = 10;
+//	public static final int LAYOUT_TYPE_SLIDE_IMAGE = 1;
+//	public static final int LAYOUT_TYPE_SLIDE_GRIDVIEW = 2;
+//	public static final int LAYOUT_TYPE_HORIZONTAL_SCROLL_VIEW = 3;
+//	public static final int LAYOUT_TYPE_HORIZONTAL_SCROLL_VIEW_SPECIAL_STORES = 4;
+//	public static final int LAYOUT_TYPE_NORMAL = 5;
+	
+	// Number of columns of Grid View
+	public static final int NUM_OF_COLUMNS_PORTRAIT = 4;
+	public static final int NUM_OF_COLUMNS_LANDSCAPE = 3;
+
+	// Gridview image padding
+	public static final int GRID_PADDING = 10; // in dp
+	
+	public static final int GRID_SPACE = 20 ; // in dp
+	
+//	public static final int GRID_HORIZONTAL_SPACE = 8 ; // in dp
+	
+//	public static final int GRID_VERTICAL_SPACE = 8; // in dp
+	
+
+	// SD card image directory
+	public static final String PHOTO_ALBUM = "Pictures/Galagala/Media/Mall-Banner";
+
+	// supported file formats
+	public static final List<String> FILE_EXTN = Arrays.asList("jpg", "jpeg",
+			"png");
+}
