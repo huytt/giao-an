4ff8d130b275c2bbf11df5d6cb9b5b83fe9e8a08
thanhhuykg@gmail.com <thanhhuykg@gmail.com@51fc294b-89fd-9dc0-b554-f689dccb7375>
2015-06-18 10:20:36 +0000

    - Add CallLogClass.java - implement getCallDetails, getcontact. - edit layout recent call. - load layout/call_layout_recent.xml

--- trunk/Gala-project/Gala_Use_Webview/gala/src/com/hopthanh/gala/app/RecentFragment.java
+++ trunk/Gala-project/Gala_Use_Webview/gala/src/com/hopthanh/gala/app/RecentFragment.java
@@ -1,28 +1,58 @@
 package com.hopthanh.gala.app;
 
 import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 
+import com.hopthanh.gala.adapter.MultiLayoutContentListViewAdapter;
+import com.hopthanh.object.CallLogClass;
+
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.CallLog;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.PhoneLookup;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.ListView;
 
 public class RecentFragment extends Fragment {
 	private static final String TAG = "RecentFragment";
 	private View mView = null;
+	private Activity mActivity = null;
+	
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+		mActivity = activity;
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		mView = inflater.inflate(R.layout.call_layout_recent, container, false);
-		String callLog = getCallDetails(inflater.getContext());
-		Log.d(TAG, callLog);
+		ListView lvRecent = (ListView) mView.findViewById(R.id.lvRecent);
+		
+//		String callLog = getCallDetails(mActivity.getApplicationContext());
+		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		java.util.Date today = Calendar.getInstance().getTime();
+		
+		MultiLayoutContentListViewAdapter adapter = new MultiLayoutContentListViewAdapter(
+				getCallDetails(mActivity.getApplicationContext()).get(df.format(today)), 
+				mActivity.getApplicationContext());
+		lvRecent.setAdapter(adapter);
+
+//		Log.d(TAG, callLog);
 		return mView;
 //		return super.onCreateView(inflater, container, savedInstanceState);
 	}
@@ -39,7 +69,85 @@ public class RecentFragment extends Fragment {
 		super.onDestroyView();
 	}
 	
-	private String getCallDetails(Context context) {
+	private HashMap<String, ArrayList<CallLogClass>> getCallDetails(Context context) {
+        HashMap<String, ArrayList<CallLogClass>> result = new HashMap<String, ArrayList<CallLogClass>>();
+//        StringBuffer sb = new StringBuffer();
+        Cursor managedCursor = context.getContentResolver().query(CallLog.Calls.CONTENT_URI, null,
+                null, null, null);
+        int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
+        HashMap<String, String> contact = getContacts(context);
+        
+//        sb.append("Call Details :");
+        while (managedCursor.moveToNext()) {
+        	String displayName = "Unsaved";
+        	boolean isSaved = false;
+            String phNumber = managedCursor.getString(number);
+        	if (contact.containsKey(phNumber)) {
+        		displayName = contact.get(phNumber);
+        		isSaved = true;
+        	}
+
+            String callType = managedCursor.getString(type);
+            String callDate = managedCursor.getString(date);
+            Date callDayTime = new Date(Long.valueOf(callDate));
+            SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+            String keyDate = df.format(callDayTime);
+            
+            String callDuration = managedCursor.getString(duration);
+            String dir = null;
+            int dircode = Integer.parseInt(callType);
+            switch (dircode) {
+            case CallLog.Calls.OUTGOING_TYPE:
+                dir = "OUTGOING";
+                break;
+
+            case CallLog.Calls.INCOMING_TYPE:
+                dir = "INCOMING";
+                break;
+
+            case CallLog.Calls.MISSED_TYPE:
+                dir = "MISSED";
+                break;
+            }
+        	CallLogClass item = new CallLogClass(displayName, phNumber, callType, callDate, callDayTime, callDuration, dir, dircode, isSaved);
+//            sb.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- "
+//                    + dir + " \nCall Date:--- " + callDayTime
+//                    + " \nCall duration in sec :--- " + callDuration);
+//            sb.append("\n----------------------------------");
+        	
+            if(!result.containsKey(keyDate)) {
+            	ArrayList<CallLogClass> value = new ArrayList<CallLogClass>();
+            	result.put(keyDate, value);
+            }
+            result.get(keyDate).add(item);
+        }
+        managedCursor.close();
+        return result;
+
+    }
+	
+	private HashMap<String, String> getContacts(Context context) {
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		Cursor contacts = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+		int nameFieldColumnIndex = contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+		int numberFieldColumnIndex = contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+		while(contacts.moveToNext()) {
+			String number = contacts.getString(numberFieldColumnIndex);
+			String name = contacts.getString(nameFieldColumnIndex);
+			result.put(number, name);
+		}
+
+		contacts.close();
+		
+		return result;
+	}
+	
+	private String getCallDetails1(Context context) {
 
         StringBuffer sb = new StringBuffer();
         Cursor managedCursor = context.getContentResolver().query(CallLog.Calls.CONTENT_URI, null,
---
+++ trunk/Gala-project/Gala_Use_Webview/gala/src/com/hopthanh/object/CallLogClass.java
@@ -0,0 +1,108 @@
+package com.hopthanh.object;
+
+import java.sql.Date;
+
+public class CallLogClass {
+    private String phNumber;
+    private String displayName;
+    private String callType;
+    private String callDate;
+    private Date callDayTime;
+    private String callDuration;
+    private String dirName;
+    private int dircode;
+    private boolean isSaved;
+    
+    public CallLogClass() {
+    	this.setDisplayName(null);
+        this.phNumber = null;
+        this.callType = null;
+        this.callDate = null;
+        this.callDayTime = null;
+        this.callDuration = null;
+        this.dirName = null;
+        this.dircode = -1;
+        this.isSaved = false;
+    }
+    
+    public CallLogClass(
+    		String displayName,
+    		String phNumber,
+    		String callType,
+    		String callDate,
+    		Date callDayTime,
+    		String callDuration,
+    		String dirName,
+    		int dirCode,
+    		boolean isSave) {
+    	this.displayName = displayName;
+        this.phNumber = phNumber;
+        this.callType = callType;
+        this.callDate = callDate;
+        this.callDayTime = callDayTime;
+        this.callDuration = callDuration;
+        this.dirName = dirName;
+        this.dircode = dirCode;
+        this.isSaved = isSave;
+    }
+    
+	public String getPhNumber() {
+		return phNumber;
+	}
+	public void setPhNumber(String phNumber) {
+		this.phNumber = phNumber;
+	}
+	public String getCallType() {
+		return callType;
+	}
+	public void setCallType(String callType) {
+		this.callType = callType;
+	}
+	public String getCallDate() {
+		return callDate;
+	}
+	public void setCallDate(String callDate) {
+		this.callDate = callDate;
+	}
+	public Date getCallDayTime() {
+		return callDayTime;
+	}
+	public void setCallDayTime(Date callDayTime) {
+		this.callDayTime = callDayTime;
+	}
+	public String getCallDuration() {
+		return callDuration;
+	}
+	public void setCallDuration(String callDuration) {
+		this.callDuration = callDuration;
+	}
+	public String getDirName() {
+		return dirName;
+	}
+	public void setDirName(String dirName) {
+		this.dirName = dirName;
+	}
+	public int getDircode() {
+		return dircode;
+	}
+	public void setDircode(int dircode) {
+		this.dircode = dircode;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public boolean isSaved() {
+		return isSaved;
+	}
+
+	public void setSaved(boolean isSaved) {
+		this.isSaved = isSaved;
+	}
+
+}
