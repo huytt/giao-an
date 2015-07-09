0969a6279ed84387314b74d893e594f881f53a7a
thanhhuykg <thanhhuykg@gmail.com>
2015-07-09 10:32:03 +0000

    resolve conflict

--- Gala-project/Gala/src/com/hopthanh/gala/app/actionbar_custom/ActionBarFragmentListener.java
+++
@@ -1,7 +0,0 @@
-package com.hopthanh.gala.app.actionbar_custom;
-
-public interface ActionBarFragmentListener {
-	void notifyUpdateActionBarFragment(AbstractActionBarFragment fragment);
-	void notifyOpenOrCloseLeftMenu();
-	void notifyStartWebViewActivity(String url);
-}
