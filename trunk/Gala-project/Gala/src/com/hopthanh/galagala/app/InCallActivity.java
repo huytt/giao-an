083bc0022612fbb55925100e0f7ef20400adb687
thanhhuykg@gmail.com <thanhhuykg@gmail.com@51fc294b-89fd-9dc0-b554-f689dccb7375>
2015-07-14 10:01:51 +0000

    - implement incall layout, incoming layout.

---
+++ trunk/Gala-project/Gala/src/com/hopthanh/galagala/app/InCallActivity.java
@@ -0,0 +1,53 @@
+package com.hopthanh.galagala.app;
+
+import com.gala.sip.SipCall;
+import com.gala.sip.SipEngine;
+import com.hopthanh.gala.utils.LayoutCallUtils;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+
+public class InCallActivity extends ActionBarActivity {
+	
+	private static final String TAG = "InCallActivity";
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.call_layout_incall_audio);
+		loadCallAction();
+	}
+	
+	private void loadCallAction(){
+		LayoutCallUtils.setCallActioinImageButton(findViewById(R.id.view_call_action_mute), R.drawable.icon_call_action_mute_80, LayoutCallUtils.TAG_AUDIO_CALL, mOnKeyboardClickListener);
+		LayoutCallUtils.setCallActioinImageButton(findViewById(R.id.view_call_action_chat), R.drawable.icon_call_action_chat_80, LayoutCallUtils.TAG_CHAT, mOnKeyboardClickListener);
+		LayoutCallUtils.setCallActioinImageButton(findViewById(R.id.view_call_action_speak), R.drawable.icon_call_action_speak_80, LayoutCallUtils.TAG_SPEAK, mOnKeyboardClickListener);
+		LayoutCallUtils.setCallActioinImageButton(findViewById(R.id.view_call_action_video_call), R.drawable.icon_call_action_video_call_80, LayoutCallUtils.TAG_VIDEO_CALL, mOnKeyboardClickListener);
+		LayoutCallUtils.setCallActioinImageButton(findViewById(R.id.view_call_action_call_transfer), R.drawable.icon_call_action_call_transfer_80, LayoutCallUtils.TAG_CALL_TRANSFER, mOnKeyboardClickListener);
+		LayoutCallUtils.setCallActioinImageButton(findViewById(R.id.view_call_action_contact), R.drawable.icon_call_action_contact_80, LayoutCallUtils.TAG_CONTACT, mOnKeyboardClickListener);
+	}
+	
+	private View.OnClickListener mOnKeyboardClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			switch(Integer.parseInt(v.getTag().toString())) {
+			case LayoutCallUtils.TAG_AUDIO_CALL:
+				break;
+			case LayoutCallUtils.TAG_CHAT:
+				break;
+			case LayoutCallUtils.TAG_SPEAK:
+				break;
+			case LayoutCallUtils.TAG_VIDEO_CALL:
+				break;
+			case LayoutCallUtils.TAG_CALL_TRANSFER:
+				break;
+			case LayoutCallUtils.TAG_CONTACT:
+				break;
+			}
+		}
+	};
+
+}
