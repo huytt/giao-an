a089d8fa9c23b249da66d5eaba461216e31c4482
thanhhuykg@gmail.com <thanhhuykg@gmail.com@51fc294b-89fd-9dc0-b554-f689dccb7375>
2015-07-15 10:04:39 +0000

    - add android:name="org.doubango.ngn.NgnApplication" to use NgnEngine.getInstance() not see the exception:  java.lang.ExceptionInInitializerError (reference: https://groups.google.com/forum/#!topic/doubango/S_v_4A46PBA)
    - Merge sipengine to SipSingleton.java.
    - Implement config and call with temp layout on FavorFragment.java.

--- trunk/Gala-project/Gala/src/com/hopthanh/galagala/app/FavorFragment.java
+++ trunk/Gala-project/Gala/src/com/hopthanh/galagala/app/FavorFragment.java
@@ -1,23 +1,143 @@
 package com.hopthanh.galagala.app;
 
+import org.doubango.ngn.events.NgnEventArgs;
+import org.doubango.ngn.events.NgnRegistrationEventArgs;
+
 import android.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FavorFragment extends Fragment {
 	private View mView = null;
+
+	private TextView mTvInfo;
+	private EditText mEtUserName;
+	private EditText mEtPassword;
+	private EditText mEtProxyHost;
+	private EditText mEtProxyPort;
+	private Button mBtSignInOut;
+	private EditText mEtDn;
+	private Button mBtDial;
+	private BroadcastReceiver mSipBroadCastRecv;
+	private SipSingleton mSipEngine = SipSingleton.getInstance();
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		mView = inflater.inflate(R.layout.call_layout_favor, container, false);
+//		mView = inflater.inflate(R.layout.call_layout_favor, container, false);
+		mView = inflater.inflate(R.layout.call_layout_setting_sip_temp, container, false);
+		
+        mTvInfo = (TextView)mView.findViewById(R.id.textViewInfo);
+        mEtUserName = (EditText)mView.findViewById(R.id.editTextPrivateIdentity);
+        mEtPassword = (EditText)mView.findViewById(R.id.editTextPassword);
+        mEtProxyHost = (EditText)mView.findViewById(R.id.editTextProxyHost);
+        mEtProxyPort = (EditText)mView.findViewById(R.id.editTextProxyPort);
+        mBtSignInOut = (Button)mView.findViewById(R.id.buttonSignInOut);
+        mEtDn = (EditText)mView.findViewById(R.id.editTextDn);
+        mBtDial = (Button)mView.findViewById(R.id.buttonDial);
+ 
+//        mEtUserName.setText(getConfigUserName());
+//        mEtPassword.setText(getConfigPassword());
+//        mEtProxyHost.setText(getConfigProxyAddr());
+//        mEtProxyPort.setText(getConfigProxyPort());
+        
+        mBtSignInOut.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if(mSipEngine.isRegistered()){
+					mSipEngine.sipUnRegister();
+				}else{
+					mSipEngine.sipConfigure(mEtUserName.getText().toString(),mEtPassword.getText().toString(),
+							mEtProxyHost.getText().toString(),mEtProxyPort.getText().toString());
+					mSipEngine.sipRegister(getActivity().getApplicationContext());
+				}
+			}
+		});
+        mBtDial.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if(mSipEngine.isRegistered()){
+					callTo(mEtDn.getText().toString());
+				}else{
+					
+				}
+			}
+		});
+        
+		// Listen for registration events
+        mSipBroadCastRecv = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				final String action = intent.getAction();
+				
+				// Registration Event
+				if(NgnRegistrationEventArgs.ACTION_REGISTRATION_EVENT.equals(action)){
+					NgnRegistrationEventArgs args = intent.getParcelableExtra(NgnEventArgs.EXTRA_EMBEDDED);
+					if(args == null){
+						mTvInfo.setText("Invalid event args");
+						return;
+					}
+					switch(args.getEventType()){
+						case REGISTRATION_NOK:
+							mTvInfo.setText("Failed to register :(");
+							break;
+						case UNREGISTRATION_OK:
+							mTvInfo.setText("You are now unregistered :)");
+							break;
+						case REGISTRATION_OK:
+							mTvInfo.setText("You are now registered :)");
+							break;
+						case REGISTRATION_INPROGRESS:
+							mTvInfo.setText("Trying to register...");
+							break;
+						case UNREGISTRATION_INPROGRESS:
+							mTvInfo.setText("Trying to unregister...");
+							break;
+						case UNREGISTRATION_NOK:
+							mTvInfo.setText("Failed to unregister :(");
+							break;
+					}
+				}
+			}
+		};
+		final IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(NgnRegistrationEventArgs.ACTION_REGISTRATION_EVENT);
+	    getActivity().getApplicationContext().registerReceiver(mSipBroadCastRecv, intentFilter);
+	    
 		return mView;
 //		return super.onCreateView(inflater, container, savedInstanceState);
 	}
 
+	
+    public void callTo(String dn)
+    {
+    	long sid = mSipEngine.prepareCall(dn);
+ 
+		Intent i = new Intent();
+		i.setClass(getActivity().getApplicationContext(), InCallActivity.class);
+		i.putExtra(InCallActivity.EXTRAT_SIPCALL_SESSION_ID, sid);
+		startActivity(i);
+
+		mSipEngine.makeCall(sid);
+    }
+
+    @Override
+    public void onResume() {
+    	// TODO Auto-generated method stub
+    	mSipEngine.onResume(getActivity().getApplicationContext());
+    	super.onResume();
+    }
+    
 	@Override
 	public void onDestroyView() {
 		// TODO Auto-generated method stub
