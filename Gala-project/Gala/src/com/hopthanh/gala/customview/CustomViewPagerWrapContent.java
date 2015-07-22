2b4221845d0c4cb431418984c012e3e054d84663
thanhhuykg <thanhhuykg@gmail.com>
2015-07-22 09:16:05 +0000

    ------------- release update 1.0.1------------------- - Fix ViewPager wrapcontent not work on android 5.0.

--- Gala-project/Gala/src/com/hopthanh/gala/customview/CustomViewPagerWrapContent.java
+++ Gala-project/Gala/src/com/hopthanh/gala/customview/CustomViewPagerWrapContent.java
@@ -19,17 +19,40 @@ public class CustomViewPagerWrapContent extends ViewPager{
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		// TODO Auto-generated method stub
-		int height = 0;
-	    for(int i = 0; i < getChildCount(); i++) {
-	        View child = getChildAt(i);
-	        child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-	        int h = child.getMeasuredHeight();
-	        if(h > height) height = h;
+	    int mode = MeasureSpec.getMode(heightMeasureSpec);
+	    // Unspecified means that the ViewPager is in a ScrollView WRAP_CONTENT.
+	    // At Most means that the ViewPager is not in a ScrollView WRAP_CONTENT.
+	    if (mode == MeasureSpec.UNSPECIFIED || mode == MeasureSpec.AT_MOST) {
+	        // super has to be called in the beginning so the child views can be initialized.
+	        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	        int height = 0;
+	        for (int i = 0; i < getChildCount(); i++) {
+	            View child = getChildAt(i);
+	            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+	            int h = child.getMeasuredHeight();
+	            if (h > height) height = h;
+	        }
+	        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
 	    }
-
-	    heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
-
+	    // super has to be called again so the new specs are treated as exact measurements
 	    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 	}
+	
+//	@Override
+//	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//		// TODO Auto-generated method stub
+//		int height = 0;
+//	    for(int i = 0; i < getChildCount(); i++) {
+//	        View child = getChildAt(i);
+//	        child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+//	        int h = child.getMeasuredHeight();
+//	        if(h > height) height = h;
+//	    }
+//	    
+//	    if(height > 0) {
+//	    	heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+//	    }
+//
+//	    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//	}
 }
